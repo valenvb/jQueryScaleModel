b668806adf70cedad16ba413b94f52cc4863d197
Valen Varangu Booth <valen@valenvb.ca>
2012-11-02 19:31:17 -0400

    more done

--- jsm.js
+++ jsm.js
@@ -2,22 +2,27 @@
 	var methods {
 		init : function (options) {
 			
+			return this.each()
+			//setup defaults
 				var settings = $.extend({
 					'page-content-div-id':'#body',
-					'custom-model-is' : '',
+					'custom-model-id' : '',
 					'speed' : '0.5',
 					
 				}, options)
-			
-			if (settings['use-custom-model']==='') {
-				$('body').append('<div id="jsm-default-model" class="jsm-def-popup">  </div>');
-			};
+				//if using the default, lets append it now.
+						if (settings['use-custom-model']==='') {
+							$('body').append('<div id="jsm-default-model" class="jsm-def-popup">  </div>');
+							//otherwise, lets find your model template now.
+						} else {
+							  jsm-your-custom-model = $(settings[csutom-model-id]);
+							 };
 			
 				};
 		
 		open : function  () {
-		  	
-				};
+		  		
+		};
 				
 		close : function () {
 		  
@@ -30,7 +35,14 @@
 
 
 	$.jsm = function (method){
-		
+		if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist!' );
+    }    
+    
 	};
 	
 	
